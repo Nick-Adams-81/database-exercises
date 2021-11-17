a16734b5f2af9b26ca2d3815bcb6a6804fcb9aa6
nick adams <thebigdeal916@icloud.com>
2021-11-17 12:14:46 -0600

    added queries for database

--- albums.sql
+++ albums.sql
@@ -3,10 +3,13 @@ USE codeup_test_db;
 
 CREATE TABLE IF NOT EXISTS albums(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(50),
-    name VARCHAR(50),
+    artist VARCHAR(50) DEFAULT 'NONE',
+    name VARCHAR(50) NOT NULL,
     release_date INT UNSIGNED,
     sales FLOAT NOT NULL,
     genre VARCHAR(50),
     PRIMARY KEY(id)
 );
+
+
+
