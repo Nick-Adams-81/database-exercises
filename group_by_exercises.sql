7144879f4c7668c5eec133acf339969205f85070
nick adams <thebigdeal916@icloud.com>
2021-11-19 14:50:51 -0600

    added count the q names

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -4,15 +4,22 @@ SELECT DISTINCT title FROM titles;
 
 SELECT last_name FROM employees
 WHERE last_name LIKE 'E%E'
-GROUP BY last_name;
+GROUP BY last_name
+ORDER BY last_name;
 
 
 SELECT DISTINCT first_name, last_name FROM employees
 WHERE last_name LIKE 'E%E'
-GROUP BY first_name, last_name;
+GROUP BY first_name, last_name
+ORDER BY last_name;
 
 
 SELECT last_name FROM employees
+WHERE last_name LIKE '%q%'
+  AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
+
+SELECT COUNT(last_name), last_name FROM employees
 WHERE last_name LIKE '%q%'
   AND last_name NOT LIKE '%qu%'
 GROUP BY last_name;
