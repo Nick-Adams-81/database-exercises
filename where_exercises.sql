14acce9e6fe3d9da61450340b73b42e6d44f225c
nick adams <thebigdeal916@icloud.com>
2021-11-19 09:28:08 -0600

    updated order by exercise

--- where_exercises.sql
+++ where_exercises.sql
@@ -3,6 +3,8 @@ USE employees;
 
 SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') AND gender = 'M';
 
+SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
+
 SELECT * FROM employees WHERE last_name LIKE 'E%E';
 
 SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
