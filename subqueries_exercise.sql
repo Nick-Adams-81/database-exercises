69d6c72f701ce488dde6aa82a2b9972a1ac88133
nick adams <thebigdeal916@icloud.com>
2021-11-23 11:25:23 -0600

    added first bonus to sub queries exercise

--- subqueries_exercise.sql
+++ subqueries_exercise.sql
@@ -18,15 +18,32 @@ WHERE first_name IN (
     FROM employees
     WHERE first_name = 'Aamod'
 );
+
 SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Name', t.title
 FROM employees AS e
          JOIN titles t
-             ON e.emp_no = t.emp_no
+              ON e.emp_no = t.emp_no
 WHERE title IN (
     SELECT title
     FROM titles
     WHERE title = 'Manager'
-    AND e.gender = 'F'
-    )
-AND t.to_date = '9999-01-01';
+      AND e.gender = 'F'
+)
+  AND t.to_date = '9999-01-01';
 
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Name', dep.dept_name, t.title
+FROM employees AS e
+         JOIN titles t
+              ON e.emp_no = t.emp_no
+         JOIN current_dept_emp AS d
+              ON e.emp_no = d.emp_no
+         JOIN departments AS dep
+              ON d.dept_no = dep.dept_no
+WHERE dept_name IN (
+    SELECT dept_name
+    FROM departments
+    WHERE title = 'Manager'
+      AND e.gender = 'F'
+)
+  AND t.to_date = '9999-01-01'
+ORDER BY dept_name;
